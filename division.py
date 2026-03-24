6037cc7854e8ae325031497d901b1120e4d74006
sakshiy0456-sudo <sakshiy0456@gmail.com>
2026-03-24 19:57:37 +0530

    Replace multiply function with divide function

--- division.py
+++ division.py
@@ -1,7 +1,10 @@
-def multiply(a, b):
-    return a * b
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-print("Multiplication =", multiply(num1, num2))
+print("Division =", divide(num1, num2))
